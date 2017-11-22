14ccd3d285dfcea27fcc0a9a76c74eada88ed3ba
user <user@users-MacBook-Pro.local>
2017-11-22 12:00:44 -0800

    added annoying popup and reset of photos when user leaves the container

--- a.js
+++ a.js
@@ -4,6 +4,16 @@ $(document).ready(function(){
         $(this).css("visibility", "hidden");
     });
 
+    $("#container").mouseleave(function(){
+        // $(img).css("visibility", "visible");
+
+        alert("HAHAHAHAHA!!! YOU CAN'T GET RID OF ME THAT EASILY");
+    });
+
+    $("#container").mouseleave(function(){
+        $("img").css("visibility", "visible");
+    });
+
     $("button").click(function(){
         $("#images img").css("visibility", "visible")
     });
